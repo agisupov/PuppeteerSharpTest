d6343ce1709d1d62ab454c1cc73529445afb6a1e
Andrei Isupov <agisupov@ya.ru>
2022-10-07 13:04:01 +0300

    Add project files

---
+++ PuppeteerSharpTest.API/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PuppeteerSharp.Media;
+using PuppeteerSharp;
+using System.Text;
+
+namespace PuppeteerSharpTest.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpPut]
+        public async Task<IActionResult> Put(IFormFile file)
+        {
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
+            await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+            await using var page = await browser.NewPageAsync();
+            await page.EmulateMediaTypeAsync(MediaType.Screen);
+            string stext = file.ReadAsString();
+            await page.SetContentAsync(stext);
+
+            var pdfContent = await page.PdfStreamAsync(new PdfOptions
+            {
+                Format = PaperFormat.A4,
+                PrintBackground = true,
+            });
+            return File(pdfContent, "application/pdf", "convertfile.pdf");
+        }
+    }
+
+    public static class Extensions
+    {
+        public static string ReadAsString(this IFormFile file)
+        {
+            var result = new StringBuilder();
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                while (reader.Peek() >= 0)
+                    result.AppendLine(reader.ReadToEnd());
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ PuppeteerSharpTest/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PuppeteerSharpTest.Models;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+
+namespace PuppeteerSharpTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public const string API_URL = "https://localhost:7220/api/Home";
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> IndexAsync(IFormFile file)
+        {
+            string url = API_URL;
+            string message = "";
+
+            using var httpClient = new HttpClient();
+            using var form = new MultipartFormDataContent();
+            using var fs = file.OpenReadStream();
+            using var streamContent = new StreamContent(fs);
+            using var fileContent = new ByteArrayContent(await streamContent.ReadAsByteArrayAsync());
+
+            fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("multipart/form-data");
+            form.Add(fileContent, "file", file.FileName);
+
+            HttpResponseMessage response = await httpClient.PutAsync(url, form);
+            if (response.IsSuccessStatusCode)
+            {
+                HttpContent content = response.Content;
+                var contentStream = await content.ReadAsStreamAsync();
+                var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(file.FileName);
+                return File(contentStream, "application/pdf", fileNameWithoutExtension + ".pdf");
+            }
+            else
+            {
+                message = response.StatusCode.ToString();
+            }
+
+            return View(message);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
